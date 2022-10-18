e669f66ff96f0470859ad6bf9bf5e24de8734d67
MatteoBallabio23 <113595415+MatteoBa23@users.noreply.github.com>
2022-10-18 12:03:53 +0200

    Versione 1.0

--- 003 - ApplicazioneClient-Server/TcpServer.java
+++ 003 - ApplicazioneClient-Server/TcpServer.java
@@ -15,6 +15,9 @@ public class TcpServer {
 		int severPort=8765;
 		int vocali=0;
 		int conso=0;
+		int num=0;
+		int caratteri=0;
+		int spazio;
 		String clientMsg = "";
 		
 		try {			 
@@ -33,39 +36,54 @@ public class TcpServer {
 			while(!clientMsg.equals("quit")) {
 				//Lettura dato da stream di rete
 				clientMsg = inStream.readUTF();
-				System.out.println("Server: ricevuto messaggio " + clientMsg );
+				System.out.println("\nServer: ricevuto messaggio " + clientMsg );
+				
+				/*
+				num=0;
+				for(int i=0;i<clientMsg.length();i++)
+					num++;
+				System.out.println("Server: lunghezza parola: "    + num);
+				*/
+				vocali=0;
+				conso=0;
+				caratteri=0;
+				spazio=0;
+				for (int i= 0; i < clientMsg.length(); i++)	//scorre il messaggio ricevuto
+				{
+					if(clientMsg.charAt(i)==('a')||clientMsg.charAt(i)==('e')||clientMsg.charAt(i)==('i')||clientMsg.charAt(i)==('o')||clientMsg.charAt(i)==('u'))
+						vocali++;
+					else if(clientMsg.charAt(i)==('!')||clientMsg.charAt(i)==('?')||clientMsg.charAt(i)==(';')||clientMsg.charAt(i)==('.')||clientMsg.charAt(i)==('-'))
+						caratteri++;
+					else if(clientMsg.charAt(i)==(' '))
+						spazio++;
+					else
+						conso++;
+				}
+			
+				System.out.println("\nServer: " + vocali + " vocali ");
+				System.out.println("Server: " + conso + " consonanti ");
+				System.out.println("Server: " + caratteri + " caratteri speciali ");
+				System.out.println("Server: " + spazio + " spazio ");
+				
+				
 				
 				//Invio dati su stream di rete
 				outStream.writeUTF("Echo from server : "         + clientMsg);
 				outStream.flush();
-				System.out.println("Server: invio messaggio "    + clientMsg );
-				
-				if(vocali/2==conso) {
-					System.out.println("Programma terminato");
-					//Chiudo il canale
-					serverSocket.close();
-					clientSocket.close();
-					inStream.close();
-					outStream.close();
-				}else {
-					for (int i= 0; i < clientMsg.length(); i++)	//scorre il messaggio ricevuto
-					{
-						if(clientMsg.equals("a")||clientMsg.equals("e")||clientMsg.equals("i")||clientMsg.equals("o")||clientMsg.equals("u"))
-							vocali++;
-						else
-							conso++;
-					}
-				}
+				System.out.println("Server: invio messaggio al client\n");
 				
 			}
 			
-			
-
 			// Close resources
 			serverSocket.close();
 			clientSocket.close();
 			inStream.close();
 			outStream.close();
+			
+			
+					
+
+			
 
 		} catch (Exception e) {
 			System.out.println(e);
