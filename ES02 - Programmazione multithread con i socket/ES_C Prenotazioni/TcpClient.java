25ad9862f6426ebda8e015318b7c526944cd0a8d
MatteoBallabio23 <113595415+MatteoBa23@users.noreply.github.com>
2022-11-15 12:03:25 +0100

    Versione 1.0

--- ES02 - Programmazione multithread con i socket/ES_C Prenotazioni/TcpClient.java
+++ ES02 - Programmazione multithread con i socket/ES_C Prenotazioni/TcpClient.java
@@ -15,7 +15,7 @@ public class TcpClient {
 
 			String severAddress="127.0.0.1";  // localhost
 			int severPort=8698;
-			int clientMessage = 0;
+			String clientMessage = "";
 			String serverMessage = "";
 			
 			// Create connection to server socket
@@ -31,17 +31,36 @@ public class TcpClient {
 			Scanner scanner = new Scanner(System.in);			
 			
 			while (!clientMessage.equals("end")) {
-
-				// Prompt user to enter some number or 'end'
+				
+		
+				
+				// inserimento della riga
 				System.out.print("Client: Seleziona la riga> ");
-				clientMessage = scanner.nextInt();
-				System.out.print("\nClient: Seleziona la colonna> ");
-				clientMessage = scanner.nextInt();
-
+				clientMessage = scanner.next();
+				try {
+					int riga = Integer.parseInt(clientMessage);
+				}catch(NumberFormatException ex) {
+					ex.printStackTrace();
+				}
 				// Send the entered number to server
-				System.out.println("Client: invio il messaggio: " + clientMessage);
+				System.out.println("Prenotazione posto");
+				System.out.println("\nClient: invio numero riga inserito: " + clientMessage);
 				outStream.writeUTF(clientMessage);
 				outStream.flush();
+				
+				//inserimento della colonna
+				System.out.print("\nClient: Seleziona la colonna> ");
+				clientMessage = scanner.next();
+				try {
+					int colonna = Integer.parseInt(clientMessage);
+				}catch(NumberFormatException ex) {
+					ex.printStackTrace();
+				}
+				// Send the entered number to server
+				System.out.println("Client: invio numero colonna inserita: " + clientMessage);
+				outStream.writeUTF(clientMessage);
+				outStream.flush();
+				
 
 				// Read data from socket input stream
 				serverMessage = inStream.readUTF();
